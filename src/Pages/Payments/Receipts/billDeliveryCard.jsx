60b9ca24f8d382149288c8a50f117d9053645efe
Raj Nainaar <karthickab77@gmail.com>
2025-04-08 20:55:58 +0530

    receipt creation

--- src/Pages/Payments/Receipts/billDeliveryCard.jsx
+++ src/Pages/Payments/Receipts/billDeliveryCard.jsx
@@ -1,15 +1,166 @@
 import { Card, CardContent, CardHeader } from "@mui/material"
+import { Addition, isEqualNumber, isValidObject, LocalDate, NumberFormat, onlynum, toNumber } from "../../../Components/functions";
+import { receiptDetailsInfo } from "./variable";
+import { useMemo } from "react";
 
 
-const DeliveryBillCard = ({ loadingOn, loadingOff, row, }) => {
+const DeliveryBillCard = ({ loadingOn, loadingOff, row = {}, receiptsPaymentInfo = [], setReceiptsPaymentInfo }) => {
+
+    const billDetails = useMemo(() => {
+        const obj = receiptsPaymentInfo.find(
+            bills => isEqualNumber(bills?.bill_id, row?.Do_Id)
+        );
+
+        return isValidObject(obj) ? obj : {};
+    }, [receiptsPaymentInfo]);
+
+    const billIndex = useMemo(() => {
+        return receiptsPaymentInfo.findIndex(
+            bills => isEqualNumber(bills?.bill_id, row?.Do_Id)
+        )
+    }, [receiptsPaymentInfo]);
+
+    const onChangeValues = (key, value) => {
+        setReceiptsPaymentInfo(pre => {
+            const newValue = [...pre];
+
+            if (billIndex === -1) {
+                const billDetails = { ...receiptDetailsInfo };
+                billDetails.bill_id = row?.Do_Id;
+                billDetails.bill_amount = row?.Amount;
+                billDetails[key] = value;
+
+                return [...newValue, billDetails];
+            } else {
+                newValue[billIndex][key] = value;
+                return newValue;
+            }
+        })
+    }
 
     return (
         <>
-            <Card>
-                <CardHeader title='' />
-                <CardContent>
+            <Card className="my-2">
+                <div className="px-3 py-2 d-flex align-items-center flex-wrap">
 
-                </CardContent>
+                    {/* {(() => {
+                        const isChecked = receiptsPaymentInfo.findIndex(o =>
+                            isEqualNumber(o?.bill_id, row?.Do_Id)
+                        ) !== -1;
+
+                        return (
+                            <div className="me-2">
+                                <input
+                                    className="form-check-input shadow-none pointer"
+                                    style={{ padding: '0.7em' }}
+                                    type="checkbox"
+                                    checked={isChecked}
+                                    onChange={() => {
+                                        // if (isChecked) changeTripDetails(arrival, true)
+                                        // else changeTripDetails(arrival)
+                                    }}
+                                />
+                            </div>
+                        )
+                    })()} */}
+
+                    <div className="flex-grow-1">
+                        <h5 className="m-0 fa-16 w-100">{row?.Do_Inv_No}</h5>
+                        <span className="fa-12 text-muted">{LocalDate(row?.Do_Date)}</span>
+                    </div>
+                    <div>
+                        <h5 className="fa-15 w-100">Pending ₹{NumberFormat(toNumber(row?.PayableValue))}</h5>
+                        <h5 className="m-0 fa-13 text-muted text-end w-100">Total ₹{NumberFormat(toNumber(row?.Total_Invoice_value))}</h5>
+                        <h5 className="m-0 fa-13 text-muted text-end w-100">Tax ₹{NumberFormat(toNumber(row?.Total_Tax))}</h5>
+                    </div>
+                </div>
+
+                <div className="table-responsive table-bordered fa-13 m-0">
+                    <table className="table m-0">
+                        <thead>
+                            <tr>
+                                {['SNo', 'Item', 'Quantity', 'Rate', 'Pack', 'Tax', 'Amount'].map((col, colInd) => (
+                                    <th key={colInd} className="bg-light">{col}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Array.isArray(row?.Products_List) && (
+                                row.Products_List.map((item, ind) => (
+                                    <tr key={ind}>
+                                        <td>{ind + 1}</td>
+                                        <td>{item?.Product_Name}</td>
+                                        <td>{item?.Bill_Qty}</td>
+                                        <td>{item?.Item_Rate}</td>
+                                        <td>{item?.UOM}</td>
+                                        <td>{Addition(item?.Cgst_Amo, item?.Sgst_Amo) || toNumber(item?.Igst_Amo)}</td>
+                                        <td>{item?.Amount}</td>
+                                    </tr>
+                                ))
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="row px-3 py-2">
+
+                    <div className="col-lg-3 col-md-4 col-sm-6 p-1">
+                        <label className="fa-14">Payment Status</label>
+                        <select
+                            value={billDetails?.payment_status ? billDetails?.payment_status : ''}
+                            onChange={e => onChangeValues('payment_status', e.target.value)}
+                            className="cus-inpt p-2"
+                        >
+                            <option value={0}>Not-verified</option>
+                            <option value={1}>verified</option>
+                        </select>
+                    </div>
+
+                    <div className="col-lg-3 col-md-4 col-sm-6 p-1">
+                        <label className="fa-14">Bank Date</label>
+                        <input
+                            type="date"
+                            value={billDetails?.bank_date ? billDetails?.bank_date : ''}
+                            onChange={e => onChangeValues('bank_date', e.target.value)}
+                            className="cus-inpt p-2"
+                        />
+                    </div>
+
+                    <div className="col-lg-3 col-md-4 col-sm-6 p-1">
+                        <label className="fa-14">Verify Status</label>
+                        <select
+                            value={billDetails?.verify_status}
+                            onChange={e => onChangeValues('verify_status', e.target.value)}
+                            className="cus-inpt p-2"
+                        >
+                            <option value={0}>Not-verified</option>
+                            <option value={1}>verified</option>
+                        </select>
+                    </div>
+
+                    <div className="col-lg-3 col-md-4 col-sm-6 p-1">
+                        <label className="fa-14">Receipt Amount (₹)</label>
+                        <input
+                            onInput={onlynum}
+                            value={billDetails?.bill_amount ? toNumber(billDetails?.bill_amount) : ''}
+                            onChange={e => onChangeValues('bill_amount', e.target.value)}
+                            className="cus-inpt p-2"
+                            placeholder="Enter Receipt Amount"
+                        />
+                    </div>
+
+                    <div className="col-12 p-1">
+                        <label className="fa-14 w-100">Narration</label>
+                        <textarea
+                            style={{ width: '100%', maxWidth: '450px'}}
+                            className="cus-inpt p-2"
+                            value={billDetails?.narration}
+                            onChange={e => onChangeValues('narration', e.target.value)}
+                            placeholder="Narration..."
+                        />
+                    </div>
+                </div>
+
             </Card>
         </>
     )
--- src/Pages/Payments/Receipts/createReceipts.jsx
+++ src/Pages/Payments/Receipts/createReceipts.jsx
@@ -1,5 +1,9 @@
 import { Button, Card, CardContent } from "@mui/material";
-import { checkIsNumber, getSessionUser, ISOString } from "../../../Components/functions";
+import {
+    checkIsNumber,
+    getSessionUser,
+    ISOString,
+} from "../../../Components/functions";
 import { fetchLink } from "../../../Components/fetchComponent";
 import Select from "react-select";
 import { useEffect, useState } from "react";
@@ -18,7 +22,11 @@ const CreateReceipts = ({ loadingOn, loadingOff }) => {
 
     const [salesPayments, setSalesPayments] = useState([]);
     const [retailers, setRetailers] = useState([]);
+    const [baseData, setBaseData] = useState({
+        salesPerson: [],
+    });
     const [receiptInfo, setReceiptInfo] = useState(receiptGeneralInfo);
+    const [receiptsPaymentInfo, setReceiptsPaymentInfo] = useState([]);
     const [filters, setFilters] = useState({
         Fromdate: ISOString(),
         Todate: ISOString(),
@@ -26,26 +34,36 @@ const CreateReceipts = ({ loadingOn, loadingOff }) => {
     });
 
     useEffect(() => {
-        fetchLink({ address: "delivery/getRetailersWhoHasBills" })
-            .then(data => {
-                if (data.success) setRetailers(data.data);
-                else setRetailers([]);
-            })
-            .catch(e => console.error(e));
+
+        fetchLink({
+            address: "delivery/getRetailersWhoHasBills"
+        }).then(data => {
+            if (data.success) setRetailers(data.data);
+            else setRetailers([]);
+        }).catch(e => console.error(e));
+
+        fetchLink({
+            address: `masters/users/salesPerson/dropDown`
+        }).then(data => {
+            if (data.success) setBaseData(pre => ({...pre, salesPerson: data.data}));
+            else setBaseData(pre => ({...pre, salesPerson: []}))
+        }).catch(e => console.error(e))
+
     }, []);
 
-    const getPendingBills = retailer => {
-        if (loadingOn) loadingOn();
-        fetchLink({ address: `delivery/retailerPendingBills?retailer_id=${retailer}` })
-            .then(data => {
+    useEffect(() => {
+        if (checkIsNumber(receiptInfo.retailer_id)) {
+            if (loadingOn) loadingOn();
+            fetchLink({
+                address: `delivery/retailerPendingBills?retailer_id=${receiptInfo.retailer_id}`,
+            }).then(data => {
                 if (data.success) setSalesPayments(data.data);
                 else setSalesPayments([]);
-            })
-            .catch(e => console.error(e))
-            .finally(() => {
+            }).catch(e => console.error(e)).finally(() => {
                 if (loadingOff) loadingOff();
             });
-    };
+        }
+    }, [receiptInfo.retailer_id])
 
     return (
         <>
@@ -53,69 +71,102 @@ const CreateReceipts = ({ loadingOn, loadingOff }) => {
                 <div className="px-3 py-2">
                     <h5 className="m-0">Receipt Creation</h5>
                 </div>
-                <CardContent>
-                    <label>Retailer</label>
-                    <div className="d-flex">
-                        <div style={{ width: '100%', maxWidth: '400px'}}>
-                            <Select
-                                menuPortalTarget={document.body}
-                                options={[
-                                    {
-                                        value: "",
-                                        label: "select",
-                                        isDisabled: true,
-                                    },
-                                    ...retailers.map(obj => ({
-                                        value: obj?.Retailer_Id,
-                                        label: obj?.Retailer_Name,
-                                    })),
-                                ]}
-                                value={filters.Retailer}
-                                menu
-                                onChange={value =>
-                                    setFilters(pre => ({
-                                        ...pre,
-                                        Retailer: value,
-                                    }))
-                                }
-                                placeholder="Search by Retailer"
-                            />
+                <form>
+                    <CardContent>
+                        <label>Retailer</label>
+                        <div className="d-flex">
+                            <div style={{ width: "100%", maxWidth: "400px" }}>
+                                <Select
+                                    menuPortalTarget={document.body}
+                                    options={[
+                                        { value: "", label: "select", isDisabled: true },
+                                        ...retailers.map(obj => ({
+                                            value: obj?.Retailer_Id,
+                                            label: obj?.Retailer_Name,
+                                        })),
+                                    ]}
+                                    value={filters.Retailer}
+                                    menu
+                                    onChange={value => {
+                                        setFilters(pre => ({ ...pre, Retailer: value }));
+                                        setReceiptInfo(pre => ({...pre, retailer_id: value.value}))
+                                    }}
+                                    placeholder="Search by Retailer"
+                                />
+                            </div>
                         </div>
-                        <Button
-                            variant="outlined"
-                            className="ms-2"
-                            startIcon={<Search />}
-                            disabled={!checkIsNumber(filters.Retailer.value)}
-                            onClick={() =>
-                                getPendingBills(filters.Retailer.value)
-                            }>
-                            Search
-                        </Button>
-                    </div>
 
-                    <div className="row fa-13">
-                        <div className="col-lg-3 col-md-4 col-sm-6 p-2">
-                            <label>Payed By</label>
-                            <input 
-                                className="cus-inpt p-2"
-                                value={receiptInfo.payed_by}
-                                onChange={e => setReceiptInfo()}
-                            />
+                        <div className="row fa-13">
+                            <div className="col-lg-3 col-md-4 col-sm-6 p-2">
+                                <label>Payed By</label>
+                                <input
+                                    className="cus-inpt p-2"
+                                    value={receiptInfo.payed_by}
+                                    placeholder="Owner, Shop Keeper.."
+                                    onChange={e => setReceiptInfo(pre => ({ ...pre, payed_by: e.target.value }))}
+                                />
+                            </div>
+
+                            <div className="col-lg-3 col-md-4 col-sm-6 p-2">
+                                <label>Date</label>
+                                <input
+                                    type="date"
+                                    className="cus-inpt p-2"
+                                    value={receiptInfo.collection_date}
+                                    onChange={e => setReceiptInfo(pre => ({ ...pre, collection_date: e.target.value }))}
+                                    required
+                                />
+                            </div>
+
+                            <div className="col-lg-3 col-md-4 col-sm-6 p-2">
+                                <label>Type</label>
+                                <select
+                                    className="cus-inpt p-2"
+                                    value={receiptInfo.collection_type}
+                                    onChange={e => setReceiptInfo(pre => ({ ...pre, collection_type: e.target.value }))}
+                                >
+                                    <option value="" disabled>Select</option>
+                                    <option value="CASH">CASH</option>
+                                    <option value="UPI">UPI</option>
+                                    <option value="CHECK">CHECK</option>
+                                    <option value="BANK ACCOUNT">BANK ACCOUNT</option>
+                                </select>
+                            </div>
+
+                            <div className="col-lg-3 col-md-4 col-sm-6 p-2">
+                                <label>Amount Received By</label>
+                                <select
+                                    className="cus-inpt p-2"
+                                    value={receiptInfo.collected_by}
+                                    onChange={e => setReceiptInfo(pre => ({ ...pre, collected_by: e.target.value }))}
+                                >
+                                    <option value="" disabled>Select</option>
+                                    {baseData.salesPerson.map((sp, spInd) => (
+                                        <option value={sp.UserId} key={spInd}>{sp.Name}</option>
+                                    ))}
+                                </select>
+                            </div>
+
                         </div>
-                        <div className="col-lg-3 col-md-4 col-sm-6 p-2"></div>
-                        <div className="col-lg-3 col-md-4 col-sm-6 p-2"></div>
-                    </div>
 
-                    {salesPayments.map((row, rowIndex) => (
-                        <DeliveryBillCard 
-                            loadingOff={loadingOff} 
-                            loadingOn={loadingOn}
-                            row={row}
-                            key={rowIndex}
-                        />
-                    ))}
-                </CardContent>
+                        
+
+                    </CardContent>
+
+                    
+                </form>
             </Card>
+
+            {salesPayments.map((row, rowIndex) => (
+                            <DeliveryBillCard
+                                loadingOff={loadingOff}
+                                loadingOn={loadingOn}
+                                row={row}
+                                key={rowIndex}
+                                receiptsPaymentInfo={receiptsPaymentInfo}
+                                setReceiptsPaymentInfo={setReceiptsPaymentInfo}
+                            />
+                        ))}
         </>
     );
 };
